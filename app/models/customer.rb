f37a4098dfa82f698af8e98310623b0b7c6fe963
Lorin Thwaits <lorin@Lorins-MacBook-Air.local>
2014-08-28 16:29:07 -0700

    Even more small changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,10 @@
 class Customer < ActiveRecord::Base
 	has_many :orders
 
+	# Show all the employees this customer has ever bought from
+	# but don't show any duplicates!
 	has_many :employees, -> { distinct }, through: :orders
   has_many :order_details, through: :orders
-  has_many :products, through: :order_details
+  has_many :products, -> { distinct }, through: :order_details
   has_many :categories, -> { distinct }, through: :products
 end
